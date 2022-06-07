e091115a653f4a09411026acb0891248993f282f
yellaiah56 <106026608+yellaiah56@users.noreply.github.com>
2022-06-07 17:28:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.255.130:9000',
+  serverUrl: 'http://43.204.110.24:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
